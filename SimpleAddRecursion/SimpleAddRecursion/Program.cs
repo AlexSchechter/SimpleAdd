d96b24d5ee4cb851b40718aacc93ed146a719570
AlexSchechter <a.schechter@outlook.com>
2016-01-02 12:33:08 +0000

    Added Recursion Version in C#

---
+++ SimpleAddRecursion/SimpleAddRecursion/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleAddRecursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input a positive integer: ");
+            SimpleAdd(Console.ReadLine());
+            
+        }
+
+        private static int SimpleAdd(string number)
+        {
+            if (number > 1)
+                number = number + SimpleAdd (number -1)
+            else
+                return number;
+        }
+    }
+
+   
+}
